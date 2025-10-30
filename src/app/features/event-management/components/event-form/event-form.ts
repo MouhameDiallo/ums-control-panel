55819790a513ac051a94800cbaaeaa8c6f7ed531
MouhameDiallo <mouhamed6244@gmail.com>
2025-10-30 17:28:53 +0000

    ajout event form

--- src/app/features/event-management/components/event-form/event-form.ts
+++ src/app/features/event-management/components/event-form/event-form.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+import {UniversityEvent} from '../../../../core/models/event.model';
+import {EventService} from '../../event.service';
 
 @Component({
   selector: 'app-event-form',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    NgIf,
+    ReactiveFormsModule
+  ],
   standalone: true,
   templateUrl: './event-form.html',
   styleUrl: './event-form.scss'
 })
-export class EventForm {
+export class EventForm implements OnInit{
+  @Input() eventToEdit!: UniversityEvent|null;
+  @Output() modalCloser = new EventEmitter<void>();
+  @Output() signalToReload = new EventEmitter<void>();
+  eventForm!: FormGroup;
+  categories: string[] = ["Tous", "Conférence", "Événement", "Atelier", "Séminaire", "Culture", "Forum"];
 
+  constructor(private eventService: EventService,
+              private fb: FormBuilder) {
+    this.eventForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(2)]],
+      description: ['', [Validators.required, Validators.minLength(2)]],
+      date: ['', [Validators.required]],
+      location: ['', [Validators.required]],
+      category: ['', Validators.required],
+      image_url: ['', ],
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.eventToEdit){
+      this.eventForm.patchValue(this.eventToEdit);
+    }
+  }
+
+  eventSubmit(): void {
+    if (this.eventForm.valid){
+      if (this.eventToEdit){
+        const eventToUpdate = this.eventForm.value
+        this.eventService.updateEvent(this.eventToEdit.id,eventToUpdate).subscribe({
+          next: () => {
+            this.signalToReload.emit();
+          },
+          error: err => {
+            console.log("Une erreur s'est produite");
+            console.error(err);
+          },
+          complete: () => this.closeModal(),
+        })
+      }else {
+        const eventToCreate = this.eventForm.value
+        this.eventService.createEvent(eventToCreate).subscribe({
+          next: value => {
+            console.log(value);
+            this.signalToReload.emit();
+          },
+          error: err => {
+            console.log("Une erreur s'est produite");
+            console.error(err);
+          },
+          complete: () => this.closeModal(),
+        })
+      }
+    }
+  }
+
+  closeModal(): void {
+    this.modalCloser.emit();
+  }
 }
